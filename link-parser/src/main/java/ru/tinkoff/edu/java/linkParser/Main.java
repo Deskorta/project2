95ae25d2f6f6b1bce62ee43c8a6cb627005930b4
Юлия Реутова <yulia.reutova@mail.ru>
2023-03-22 01:01:24 +0500

    commit 2

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkParser/Main.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkParser/Main.java
@@ -1,7 +1,24 @@
 package ru.tinkoff.edu.java.linkParser;
 
+import java.net.MalformedURLException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws MalformedURLException {
+        LinkParser githubParser = new GithubParser();
+        LinkParser stackOverflowParser = new StackOverflowParser();
+        githubParser.setNextParser(stackOverflowParser);
+
+        String[] githubResult = githubParser.parse("https://github.com/sanyarnd/tinkoff-java-course-2022/");
+// githubResult = {"sanyarnd", "tinkoff-java-course-2022"}
+
+        String[] stackOverflowResult = githubParser.parse("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c");
+// stackOverflowResult = {"1642028"}
+
+        String[] unsupportedResult = githubParser.parse("https://unsupported.com");
+// unsupportedResult = null
+        System.out.println(Arrays.toString(githubResult));
+        System.out.println(Arrays.toString(stackOverflowResult));
+        System.out.println(Arrays.toString(unsupportedResult));
     }
 }
